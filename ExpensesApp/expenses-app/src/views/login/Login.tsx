0176e8dca73993b309f579a00b0fcd308e4e2353
Marco <marco.sampaio@atlanse.pt>
2024-08-24 09:44:04 +0100

    login and routing

---
+++ ExpensesApp/expenses-app/src/views/login/Login.tsx
@@ -0,0 +1,52 @@
+import InputComponent from "../../utils/components/input-component/input-component";
+import styles from "./login.module.css";
+import logo from "../../assets/expenses_logo.png";
+import PrimaryButton from "../../utils/components/primary-button/primary-button";
+import { useEffect, useState } from "react";
+import LoginForm from "./form/login-form";
+import { useNavigate } from "react-router-dom";
+
+
+interface LoginProps {
+  loggedIn: boolean;
+  setLoggedIn: (value: boolean)=> void
+}
+
+
+export default function Login({loggedIn, setLoggedIn}: LoginProps) {
+
+  const navigate = useNavigate();
+  useEffect (() => {
+    if(loggedIn) navigate('dasboard');
+  });
+
+  return (
+    <main>
+      <header>
+        <h3>Expenses App</h3>
+      </header>
+      <body
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div className={styles.container}>
+          <div className={styles.title}>
+            <h1>
+              Welcome to the best <br />
+              Expense
+              <br /> tracker application
+            </h1>
+            <img src={logo} width={120} height={120} alt="logo" />
+          </div>
+          <h4>
+            Start by login into your account and track every cent that you spend
+            and earn.
+          </h4>
+          <LoginForm setLoggedIn={setLoggedIn}/>
+        </div>
+      </body>
+    </main>
+  );
+}
